f96e601f40aa03846aaaa6aa76776177b77ec1ba
Yura <khristenkoyura@gmail.com>
2015-03-09 15:17:53 +0300

    change change example

--- README.md
+++ README.md
@@ -16,4 +16,5 @@ The recommended way to install reactphp-curl is through [composer](http://getcom
 ## Introduction
 
 This is a multi curl driver for [reactphp](https://github.com/reactphp/react)
-Examples show in folder /example/
+
+Example show in folder /example/
--- example/simple.php
+++ example/simple.php
@@ -1,14 +1,10 @@
 <?php
 
 use \React\EventLoop\Factory;
-use \React\Curl\Client;
+use \React\Curl\Curl;
 use \React\Curl\Exception;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-
-$loop = Factory::create();
-$client = new Client($loop);
 
 $cb_ok = function(multiCurl\Result $result){
     echo $result->info['url'], PHP_EOL;
@@ -20,19 +16,41 @@ $cb_err = function(Exception $e){
 };
 
 
-$client->get('https://www.google.ru/')->then($cb_ok, $cb_err);
-$client->get('http://www.yandex.ru/', [CURLOPT_REFERER => 'https://google.ru/'])->then($cb_ok, $cb_err);
+$loop = Factory::create();
+$curl = new Curl($loop);
 
-$client->get('http://yandex.ru/404.html', [CURLOPT_REFERER => 'https://google.ru/'])->then($cb_ok, $cb_err);
+// Config
+$curl->client->setMaxRequest(3);
+$curl->client->setSleep(6, 1.0, false); // 6 request in 1 second
+$curl->client->setCurlOption([CURLOPT_AUTOREFERER => true, CURLOPT_COOKIE => 'fruit=apple; colour=red']); // default options
+
+// More config $curl->client show https://github.com/KhristenkoYura/multicurl
+// endconfig
+
+//get result 
+$curl->get('https://graph.facebook.com/http://www.yandex.ru')->then(function($result){
+	echo (string) $result, PHP_EOL; // echo $result->body; OR echo $result->getBody();
+});
+
+// get json
+$curl->get('https://graph.facebook.com/http://google.com')->then(function($result){
+	print_r($result->json);
+	// print_r($result->getJson(true)); -> print array
+});
+
+$curl->get('https://www.google.ru/')->then($cb_ok, $cb_err);
+$curl->get('http://www.yandex.ru/', [CURLOPT_REFERER => 'https://google.ru/'])->then($cb_ok, $cb_err);
+
+$curl->get('http://yandex.ru/404.html', [CURLOPT_REFERER => 'https://google.ru/'])->then($cb_ok, $cb_err);
 
 
 
-$client->add([CURLOPT_URL => 'http://www.yandex.ru/'])->then($cb_ok, $cb_err);
-$client->add([CURLOPT_URL => 'https://google.ru/'])->then($cb_ok, $cb_err);
+$curl->add([CURLOPT_URL => 'http://www.yandex.ru/'])->then($cb_ok, $cb_err);
+$curl->add([CURLOPT_URL => 'https://google.ru/'])->then($cb_ok, $cb_err);
 //Run request in method add
-$client->run();
+$curl->run();
 
-$client->post('http://www.yandex.ru/post', ['post-key' => 'post-value'])->then($cb_ok, $cb_err);
+$curl->post('http://www.yandex.ru/post', ['post-key' => 'post-value'])->then($cb_ok, $cb_err);
 
 
 $loop->run();
